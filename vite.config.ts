da77e67b5789d3dffb6cc085440a8be019571d0b
GilangNyan <sagilang@gmail.com>
2024-07-08 14:49:00 +0700

    refactor: install leaflet for map and driver tracking

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/absensi': {
+      '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
         ws: true
