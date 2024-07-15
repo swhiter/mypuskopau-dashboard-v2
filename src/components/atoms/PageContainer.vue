f316cfe072eef0feb018242b379e89524884b9d4
GilangNyan <sagilang@gmail.com>
2024-07-15 07:47:02 +0700

    feat: main page cards

---
+++ src/components/atoms/PageContainer.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="container">
+    <h3 class="title">{{ title }}</h3>
+    <slot />
+  </main>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: ''
+})
+</script>
+
+<style scoped>
+.container {
+  @apply font-body p-6 space-y-6
+}
+
+.title {
+  @apply text-xl text-accentGray font-medium
+}
+</style>
---
+++ src/components/atoms/PageInnerCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="space-y-1">
+    <h5 class="card-title">{{ title }}</h5>
+    <div class="container">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: ''
+})
+</script>
+
+<style scoped>
+.container {
+  @apply flex flex-col w-full p-4 bg-white rounded-lg border border-buttonGray shadow-lg
+}
+
+.card-title {
+  @apply font-semibold text-lg
+}
+</style>
