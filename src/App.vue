55e03b964d584f3c11c0cce4dc034311921fe885
GilangNyan <sagilang@gmail.com>
2024-07-15 07:47:29 +0700

    feat: route guards

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,34 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import '@/utils/validationRules'
+import 'leaflet/dist/leaflet.css'
+import router from './router';
+import { useAuthStore } from './stores/auth';
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    requiresAuth: boolean
+  }
+}
+
+const authStore = useAuthStore()
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!authStore.token) {
+      next({
+        path: '/login',
+        query: {
+          next: to.fullPath
+        }
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
 </script>
 
 <template>
